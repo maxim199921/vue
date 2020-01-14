d283a1034637e36cc295e9ed4c86a492be20e0a8
Chupilin Maksim <m.chupilin@vironit.co.uk>
2020-01-14 09:36:10 +0300

    chechbox

---
+++ select/index.js
@@ -0,0 +1,37 @@
+const form = document.forms["testForm"];
+form.addEventListener('submit', sendForm);
+
+const checkboxes = document.getElementsByName("favorite_pet");
+const other = document.querySelector("[value='other']");
+
+if (other) {
+    for(let i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].addEventListener('change', function (event) {
+            if (event.target.value !== 'other') {
+                other.checked = false;
+            } else if (other.checked) {
+                for(let i = 0; i < checkboxes.length; i++) {
+                    if (other !== checkboxes[i]) {
+                        checkboxes[i].checked = false;
+                    }
+                }
+            }
+        });
+    }
+}
+
+function sendForm(e) {
+    let favorite_pet = [];
+    for(let i = 0; i < checkboxes.length; i++) {
+        if(checkboxes[i].checked) {
+            favorite_pet.push(checkboxes[i].value);
+        }
+    }
+    e.preventDefault();
+    executeAjax(favorite_pet)
+}
+
+function executeAjax(data) {
+    console.log(data);
+    //AJAX REQUEST
+}
