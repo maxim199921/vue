16081247de6cecfbe19503ff766aa75dec92c2ff
Chupilin Maksim <m.chupilin@vironit.co.uk>
2020-01-17 09:32:05 +0300

    xxx

---
+++ dinemicComponents/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <app-quote>
+                    <app-author></app-author>
+                    <app-new></app-new>
+                    <h2 slot="title">{{ quoteTitle }}</h2>
+                    <p>A wonderful Quote</p>
+                </app-quote>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './components/Quote.vue';
+    import Author from './components/Author.vue';
+    import New from './components/New.vue';
+
+    export default {
+        data: function() {
+            return {
+                quoteTitle: 'The Quote',
+                selectedComponent: 'appQuote'
+            }
+        },
+        components: {
+            appQuote: Quote,
+            appAuthor: Author,
+            appNew: New
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ dinemicComponents/src/components/New.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h3>new quote</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid #ccc;
+        box-shadow: 1px 1px 2px black;
+        padding: 30px;
+        margin: 30px auto;
+        text-align: center;
+    }
+</style>
