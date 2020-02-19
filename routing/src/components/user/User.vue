7d1efc719ca0f697437dcfcd45d91bcfe39f4c92
ChupilinMaksimgit config --global user.name ChupilinMaksim <M.Chupilin@sam-solutions.com>
2020-02-19 10:59:17 +0300

    routing

--- routing/src/components/user/User.vue
+++ routing/src/components/user/User.vue
@@ -1,4 +1,35 @@
 <template>
-    <h1>The User Page</h1>
-
+    <div>
+        <h1>The User Page</h1>
+        <hr>
+        <p>id: {{ id }}</p>
+        <button @click="navigateToHome">Go Home</button>
+        <hr>
+        <router-view></router-view>
+    </div>
 </template>
+
+<script>
+    export default {
+        data() {
+          return {
+              id: this.$route.params.id
+          }
+        },
+        watch: {
+            '$route'(to, from) {
+                console.log(to);
+                console.log(from);
+                this.id = to.params.id;
+            }
+        },
+        methods: {
+            navigateToHome() {
+                this.$router.push('/')
+            }
+        },
+        created() {
+            console.log(this.id)
+        }
+    }
+</script>
--- routing/src/components/user/UserDetail.vue
+++ routing/src/components/user/UserDetail.vue
@@ -1,3 +1,7 @@
 <template>
-    <h3>Some User Details</h3>
+    <div>
+        <h3>Some User Details</h3>
+        <p>User: {{ $route.params.id }}</p>
+        <router-link tag="button" :to="`/user//edit`">Edit</router-link>
+    </div>
 </template>
