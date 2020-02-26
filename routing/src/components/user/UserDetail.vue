2ccfcba855e105bbcd4e256d74ee4f1551f51c3e
ChupilinMaksimgit config --global user.name ChupilinMaksim <M.Chupilin@sam-solutions.com>
2020-02-26 11:50:56 +0300

    routing

--- routing/src/components/user/UserDetail.vue
+++ routing/src/components/user/UserDetail.vue
@@ -2,6 +2,6 @@
     <div>
         <h3>Some User Details</h3>
         <p>User: {{ $route.params.id }}</p>
-        <router-link tag="button" :to="`/user//edit`">Edit</router-link>
+        <router-link tag="button" :to="{ name: 'userName', params: { id: $route.params.id } }">Edit user</router-link>
     </div>
 </template>
